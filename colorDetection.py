6919e2f10e3bbdda2e2da6020eac62af21800e18
pandey.ramu <pandey.ramu62@gmail.com>
2023-01-22 17:28:30 +0100

    modified:   colorDetection.py 	new file:   colored_pic.jpg

--- colorDetection.py
+++ colorDetection.py
@@ -1,23 +1,18 @@
-#Import necessary libraries
 import numpy as np
 import pandas as pd
-import cv3
+import cv2
 import argparse
 
-#Get Input image from the user using CommandPrompt/Terminal
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required = True, help = "Image Path")
 args = vars(ap.parse_args())
 img_path = args['image']
 
-#Read with opencv
 img = cv2.imread(img_path)
 imgWidth = img.shape[1] - 40
 
-#declaring global variables (are used later on)
 r = g = b = xpos = ypos = 0
 
-#Read the color dataset
 index = ['color', 'color_name', 'hex', 'R', 'G', 'B']
 data = pd.read_csv('colors.csv', names = index, header = None)
 
@@ -52,7 +47,6 @@ while(1):
 	text = getColorName(r,g,b) + '   R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
 	#cv2.putText(img,text,start,font(0-7), fontScale, color, thickness, lineType, (optional bottomLeft bool) )
 	cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-	#For very light colours we will display text in black colour
 	if(r+g+b >= 600):
 		cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
 
